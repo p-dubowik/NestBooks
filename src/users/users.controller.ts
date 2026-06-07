e2d98862ebcaf3c96c91dbfd4d0ca9f3a5c0da86
Paweł Dubowik <p.dubowik27@gmail.com>
2026-06-07 18:19:42 +0200

    Add auth-admin guard. Introduce DELETE endpoint in users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, NotFoundException, Param, ParseUUIDPipe, UseGuards } from '@nestjs/common';
+import { Controller, Get, Delete, NotFoundException, Param, ParseUUIDPipe, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { AdminAuthGuard } from 'src/auth/admin-auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -18,5 +19,15 @@ export class UsersController {
         const user = await this.userservice.getById(id);
         if(!user) throw new NotFoundException('User not found');
         return user;
+    }
+
+    @Delete('/:id')
+    @UseGuards(AdminAuthGuard)
+    @UseGuards(JwtAuthGuard)
+    public async deleteById(@Param('id', new ParseUUIDPipe()) id: string) {
+        const user = await this.userservice.getById(id);
+        if(!user) throw new NotFoundException('User not found')
+        await this.userservice.deleteById(id);
+        return { success: true };
     }
 }
